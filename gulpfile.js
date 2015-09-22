3b4a71086f7852181ecb32b5bf0286dbf64c1113
Scott Hatcher <scott@scotthatcher.com>
2015-09-21 19:48:56 -0700

    Created gh-pages demo site.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var useref = require('gulp-useref');
+var ghPages = require('gulp-gh-pages');
+var concat = require('gulp-concat');
+var replace = require('gulp-replace');
+
 
 gulp.task('serve', function() {
-    gulp.src('.')
+    return gulp.src('.')
         .pipe(webserver({
             livereload: true,
             //directoryListing: true,
@@ -22,8 +26,41 @@ gulp.task('useref', function () {
 });
 
 gulp.task('html', function() {
-    gulp.src('./app/**/*.html')
+    return gulp.src('./app/**/*.html')
         .pipe(gulp.dest('./dist/app'));
 });
 
 gulp.task('build', ['useref', 'html']);
+
+gulp.task('copy-demo', ['build'], function() {
+    return gulp.src('./dist/**/**.*')
+        .pipe(gulp.dest('./demo'));
+
+});
+gulp.task('build-demo', ['copy-demo', 'offline-scripts'], function() {
+    return gulp.src('./dist/**/**.*')
+        .pipe(gulp.dest('./demo'));
+
+});
+
+gulp.task('offline-scripts', function() {
+    return gulp.src([
+            'offline/apCachedXML.js',
+            'bower_components/angular-point/test/mock/apMockBackend.mock.js',
+            'bower_components/angular-mocks/angular-mocks.js'
+        ])
+        .pipe(concat('offline.js'))
+        .pipe(gulp.dest('./demo/'));
+});
+
+gulp.task('inject-demo-scripts', ['build-demo'], function(){
+    return gulp.src(['demo/index.html'])
+        .pipe(replace('<!--DEMO-BUILD-SCRIPTS-->', '<script src="offline.js"></script>'))
+        .pipe(gulp.dest('demo/'));
+});
+
+/** Deploy to gh-pages branch */
+gulp.task('deploy-demo', ['inject-demo-scripts'], function(){
+    return gulp.src('./demo/**/*')
+        .pipe(ghPages());
+});
